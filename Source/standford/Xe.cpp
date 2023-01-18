3eef7bc017fdd77a00b860d1b0cff9ef3fccd65e
华 马 <841582037@qq.com>
2023-01-18 20:02:55 +0800

    添加项目文件。

---
+++ Source/standford/Xe.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Xe.h"
+#include "Camera/CameraComponent.h"
+#include "GameFrameWork/SpringArmComponent.h"
+// Sets default values
+AXe::AXe()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	cespringarm = CreateDefaultSubobject<USpringArmComponent>("cesare_springarm");
+	cecamera = CreateDefaultSubobject<UCameraComponent>("cesare_camera");
+}
+
+// Called when the game starts or when spawned
+void AXe::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AXe::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AXe::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/standford/standfordGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "standfordGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STANDFORD_API AstandfordGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
